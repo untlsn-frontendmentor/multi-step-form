2853be41b1170e427638a6cd93ba796ddd424921
Filip Skoczeń <PhilipSkoczen@gmail.com>
2023-10-14 11:11:40 +0200

    Add Page5

--- app.vue
+++ app.vue
@@ -24,9 +24,9 @@ const formsValues = ref<[FirstPageFormInputs, SecondPageFormInputs, boolean[]]>(
   <main class="bg-[#EEF5FF] min-h-screen">
     <article class="bg-white rounded-xl p-4 flex justify-stretch w-max mx-auto">
       <FormAside />
-
+      <LazySubformsPage5 v-if="page == 5" />
       <LazySubformsPage4
-        v-if="page == 4"
+        v-else-if="page == 4"
         :data="formsValues"
       />
       <LazySubformsPage3 v-else-if="page == 3" v-model:values="formsValues[2]" :yearly="formsValues[1].isYearly" />
---
+++ components/subforms/Page5.vue
@@ -0,0 +1,20 @@
+<script setup lang="ts">
+</script>
+
+<template>
+  <main class="flex-(~ col) justify-center items-center gap-4 text-center max-w-160 p-16 h-130">
+    <img src="/images/icon-thank-you.svg" alt="">
+    <h1 class="font-bold text-4xl mt-4">
+      Thank you!
+    </h1>
+    <p>
+      Thanks for confirming your subscription! We hope you have fun
+      using our platform. If you ever need support, please feel free
+      to email us at support@loremgaming.com.
+    </p>
+  </main>
+</template>
+
+<style scoped>
+
+</style>
